0577d2b1d97108b619d4ee123cd1aa1b51815591
kenlsm <kenlsm@outlook.com>
2021-04-14 23:28:40 +0800

    chore: remove unused ecosystem comments

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -15,17 +15,4 @@ module.exports = {
     watch: 'src/db',
     ignore_watch: ['src/db/**/stores']
   }],
-
-  // deploy : {
-  //   production : {
-  //     user : 'SSH_USERNAME',
-  //     host : 'SSH_HOSTMACHINE',
-  //     ref  : 'origin/master',
-  //     repo : 'GIT_REPOSITORY',
-  //     path : 'DESTINATION_PATH',
-  //     'pre-deploy-local': '',
-  //     'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production',
-  //     'pre-setup': ''
-  //   }
-  // }
 };
