d87047ddef596483046e360f871a803458652a8f
kenlsm <kenlsm@outlook.com>
2021-04-18 12:43:23 +0800

    server: complete search route

--- src/server/routes/index.ts
+++ src/server/routes/index.ts
@@ -1,10 +1,16 @@
+import fetch from 'node-fetch';
 import { Express } from 'express';
 
+import { BASE_URL } from '../Constants';
+
 export default (app: Express) => {
-  app.get('/api/search', (req, res) => {
+  app.get('/api/search', async (req, res) => {
     const { query } = req;
     const { restaurants, time } = query;
     if (query.restaurants) {
+      const response = await fetch(BASE_URL + `/CMD_GET_RESTAURANTS_BY_NAME?name=${query.restaurants}`);
+
+      return res.json(await response.json());
     }
     return res.json({ query });
   });
