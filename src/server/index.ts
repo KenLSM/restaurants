6f56708624a1c850c24d4dfae20b8575237c10fa
kenlsm <kenlsm@outlook.com>
2021-04-18 13:40:39 +0800

    server: fix cors

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,11 +1,12 @@
 import express from 'express';
 import morgan from 'morgan';
 import routes from './routes';
+import cors from 'cors';
 
 const PORT = 8081;
 
 const app = express();
-
+app.use(cors());
 app.use(morgan('combined'));
 
 routes(app);
